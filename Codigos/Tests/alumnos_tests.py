7fd0952ce36037606988252843bfa263811dcf8d
Arian Giles García <arian2822@gmail.com>
2016-07-04 16:05:50 -0300

    Agrego tests sobre alumnos

---
+++ Codigos/Tests/alumnos_tests.py
@@ -0,0 +1,12 @@
+import unittest
+from Codigos.Scripts_databases.Operaciones_alumnos import *
+
+
+class AutenticacionTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
