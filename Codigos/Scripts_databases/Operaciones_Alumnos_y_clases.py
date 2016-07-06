badb1b02fde0b41aed621526a8aa7e299be3e74a
Arian Giles García <arian2822@gmail.com>
2016-07-06 11:15:56 -0300

    Agregue returns en metodos

--- Codigos/Scripts_databases/Operaciones_Alumnos_y_clases.py
+++ Codigos/Scripts_databases/Operaciones_Alumnos_y_clases.py
@@ -1,25 +1,30 @@
 import mysql.connector
 
+
 def agregar_alumno_y_clase(db,id_alumno,id_clase):
-
-    #Primero obtengo el cursor de la db
-
     try:
         cursor = db.cursor()
         print("la base de datos se abrio correctamente")
     except Exception as e:
         print("Error al abrir la base de datos, en el metodo agregar_alumno_y_clase ->"+str(e))
 
-    #Inserto un nuevo docente con los parametros dados.
+    # Inserto un nuevo alumno/clase con los parametros dados.
     try:
         cursor.execute("INSERT INTO alumnos_y_clases\
           (id_alumno,id_clase)\
           VALUES({},{})".format(id_alumno,id_clase))
         print("El alumno y clase se inserto correctamente")
-        # Comiteo los cambios a la base de datos.
         db.commit()
+
+        return True
     except Exception as e:
-        print("Error al insertar alumno y clase, en el metodo agregar_alumno_y_clase -> " + str(e))
+        raise ErrorAgregarAlumnoClase
+
+
+class ErrorAgregarAlumnoClase(Exception):
+    def __str__(self):
+        return "No se pudo agregar el alumno/clase"
+
 
 if __name__ == "__main__":
     database = mysql.connector.connect(user='root', password='root', host='127.0.0.1',database='academia')
